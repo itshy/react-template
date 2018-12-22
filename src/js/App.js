c56eed8438b93ef41b2910f01a71679c747459fe
itshy <ilya07081999@gmail.com>
2018-12-22 13:12:01 +0300

    first commit

---
+++ src/js/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react-dom'
+
+import HomePage from './containers/HomePage'
+
+export default class App extends Component {
+
+  render () {
+
+    return (
+
+      <HomePage />
+
+    )
+
+  }
+
+}
